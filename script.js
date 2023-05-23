3ac04296f148c49f8566035d3c32a9fcb29c4c24
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-23 13:37:01 +0500

    script.js
    length find string

---
+++ script.js
@@ -0,0 +1,21 @@
+// string lenght find
+// var abc="i love pakistan my country is beautiful place"
+// var getstringlength=abc.length
+// console.log(getstringlength);
+// var array1=["kashif","javaid","ali","abdullah","ramzan"];
+// for (let index = 0; index < array1.length; index++) {
+//     const getstringlength = array1[index];
+//     console.log(index);
+//     console.log(getstringlength);
+    
+// }
+// slice
+// var abc="i,love,pakistan,my country is beautiful place";
+// var elemant= abc.slice(7,15)
+// console.log(elemant);
+
+// replace word
+
+var abc="i,love,pakistan,my country is beautiful place";
+var elemant= abc.replace("pakistan","PAKISTAN")
+console.log(elemant);
